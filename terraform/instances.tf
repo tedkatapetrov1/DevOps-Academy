ad1c41e0dba5064eab983c36d8debb935ba09043
tedkatapetrov1 <teodor.petrov@abv.bg>
2023-06-16 13:55:22 +0300

    new files

---
+++ terraform/instances.tf
@@ -0,0 +1,25 @@
+
+# DATA #############################################################
+
+data "aws_ssm_parameter" "ami" {
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
+}
+
+# INSTANCES #
+resource "aws_instance" "nginx1" {
+  ami                    = nonsensitive(data.aws_ssm_parameter.ami.value)
+  instance_type          = var.instance_type
+  subnet_id              = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.nginx-sg.id]
+
+  user_data = <<EOF
+#! /bin/bash
+sudo amazon-linux-extras install -y nginx1
+sudo service nginx start
+sudo rm /usr/share/nginx/html/index.html
+echo '<html><style>body {font-size: 20px;}</style><body><p>Ace!! &#x1F0A1;</p></body></html>' | sudo tee /usr/share/nginx/html/index.html
+EOF
+
+  tags = local.common_tags
+
+}
